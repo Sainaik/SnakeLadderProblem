b3533b37033b4e7e7bef4b63614a4d4ac9710820
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-29 21:13:12 +0530

    Reverted. Added player and start postion as 0

--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -7,6 +7,17 @@ namespace SnakeLadderProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello  Welcome to Snake Ladder Problem!");
+            
+            //Constants
+
+            const int INITIAL_POS = 0;
+
+            //variables
+
+            int position = 0;
+
+            Console.Out.WriteLine("Start position is: " + INITIAL_POS);
+
         }
     }
 }
