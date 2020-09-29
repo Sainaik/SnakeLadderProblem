3cb499a35406919bbd4f56e99122ea9669c0ab9e
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-29 21:39:16 +0530

    Reverted. Added Two players and Printed with winner

--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -4,34 +4,23 @@ namespace SnakeLadderProblem
 {
     class Program
     {
-        public static int GetPositon(int start, int end)
+        const int NO_Play = 0;
+        const int Ladder = 1;
+        const int Snake = 2;
+
+        public static int PlayGame(String player, int prev_postion)
         {
-            Random rand = new Random();
+            // variables
 
-            return rand.Next(start, end);
-        }
-        static void Main(string[] args)
-        {
-
-            //Constants
-
-            const int NO_Play = 0;
-            const int Ladder = 1;
-            const int Snake = 2;
-
-
-            //variables
-
-
-            int position = 0;
+            int position = prev_postion;
             int next_position = 0;
             int play_option = 0;
-            int count1 = 0;
+            bool ladder = true;
 
+            // while ladder is true player continues the game 
+            // or he quits
 
-
-
-            while (position != 100)
+            while (ladder == true)
             {
                 next_position = GetPositon(0, 7);
 
@@ -40,31 +29,82 @@ namespace SnakeLadderProblem
                 switch (play_option)
                 {
                     case NO_Play:
-                        Console.Out.WriteLine("NO_PLAY, ur postion is " + position);
+                        Console.Out.WriteLine(player + " NO_PLAY, ur postion is " + position);
+                        ladder = false;
                         break;
                     case Ladder:
                         position += next_position;
-                        if (position > 100)
+                        ladder = true;
+                        if (position <= 100)
                         {
-                            position -= next_position;
+                            Console.Out.WriteLine(player + " Moved forward to " + position);
+
                             break;
                         }
-                        Console.Out.WriteLine("Moved forward to " + position);
+                        else
+                        {
+                            position -= next_position;
+                            Console.Out.WriteLine(player + " Moved forward to " + position);
+                        }
                         break;
                     case Snake:
                         position -= next_position;
                         if (position <= 0)
                         {
                             position = 0;
+
                         }
-                        Console.Out.WriteLine("Moved backward to " + position);
+                        Console.Out.WriteLine(player + " Moved backward to " + position);
+                        ladder = false;
                         break;
                     default:
+                        ladder = false;
                         break;
                 }
-                count1++;
             }
-            Console.Out.WriteLine("The dice is rolled " + count1 + " times to reach 100");
+
+            return position;
         }
+        public static int GetPositon(int start, int end)
+        {
+            Random rand = new Random();
+
+            return rand.Next(start, end);
+        }
+        static void Main(string[] args)
+        {
+            String player1 = "Player1";
+            String player2 = "Player2";
+
+
+            //variables
+
+            int position1 = 0;
+
+            int position2 = 0;
+
+
+            while ((position1 != 100) || (position2 != 100))
+            {
+                position1 = PlayGame(player1, position1);
+
+                if (position1 == 100)
+                {
+                    break;
+                }
+                position2 = PlayGame(player2, position2);
+            }
+
+            if (position1 == 100)
+            {
+                Console.Out.WriteLine("Player1 won the game");
+            }
+            else
+            {
+                Console.Out.WriteLine("Player2 won the game");
+            }
+
+        }
+
     }
 }
