26ecd74616203ceb86a26ddbb71ba05decd15aeb
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-29 21:24:05 +0530

    Reverted. Player checks for play option and changes postion

--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -4,31 +4,53 @@ namespace SnakeLadderProblem
 {
     class Program
     {
-        public static int GetPositon()
+        public static int GetPositon(int start, int end)
         {
             Random rand = new Random();
 
-            return rand.Next(0, 7);
+            return rand.Next(start, end);
         }
         static void Main(string[] args)
         {
 
+            const int NO_Play = 0;
+            const int Ladder = 1;
+            const int Snake = 2;
 
-            //Constants
-
-            const int INITIAL_POS = 0;
 
             //variables
 
+
             int position = 0;
+            int next_position = 0;
+            int play_option = 0;
+
+           
+            next_position = GetPositon(0, 7);
+
+            play_option = GetPositon(0, 3);
 
 
-            Console.Out.WriteLine("Start position is: " + INITIAL_POS);
-
-            position = INITIAL_POS + GetPositon();
-
-            Console.Out.WriteLine("After the first roll: " + position);
-
+            switch (play_option)
+            {
+                case NO_Play:
+                    Console.Out.WriteLine("NO_PLAY, ur postion is " + position);
+                    break;
+                case Ladder:
+                    position += next_position;
+                    Console.Out.WriteLine("Moved forward to " + position);
+                    break;
+                case Snake:
+                    position -= next_position;
+                    if (position <= 0)
+                    {
+                        position = 0;
+                    }
+                    Console.Out.WriteLine("Moved backward to " + position);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
