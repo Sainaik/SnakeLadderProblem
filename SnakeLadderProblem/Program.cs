7758bec0e9805370ecda04b5df33238ddcc53e65
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-09-29 21:31:53 +0530

    Reverted. player gets exact number 100

--- SnakeLadderProblem/Program.cs
+++ SnakeLadderProblem/Program.cs
@@ -26,6 +26,7 @@ namespace SnakeLadderProblem
             int position = 0;
             int next_position = 0;
             int play_option = 0;
+            //int count1 = 0;
 
 
 
@@ -61,8 +62,9 @@ namespace SnakeLadderProblem
                     default:
                         break;
                 }
-
+                //count1++;
             }
+            //Console.Out.WriteLine("The dice is rolled " + count1 + " times to reach 100");
         }
     }
 }
